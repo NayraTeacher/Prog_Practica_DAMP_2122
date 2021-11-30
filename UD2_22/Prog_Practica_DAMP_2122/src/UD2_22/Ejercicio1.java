e3d2a8bf485f3e2d6a8b0d2e60783719f55f489a
nayra.deniz <nayra.deniz@UTWKS0652.liveu-tad.es>
2021-11-30 14:05:33 +0100

    Primera version

---
+++ UD2_22/Prog_Practica_DAMP_2122/src/UD2_22/Ejercicio1.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package UD2_22;
+
+/**
+ * @author nayra.deniz
+ *
+ */
+public class Ejercicio1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hola clase, primera prueba github");
+
+	}
+
+}
